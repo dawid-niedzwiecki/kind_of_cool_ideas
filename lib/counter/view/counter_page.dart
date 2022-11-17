5b84e957882551f28896139ea622b38c8a948c07
Dawid Niedźwiecki <dawid.niedzwiecki@neptis.pl>
2022-11-17 22:56:06 +0100

    Initial project configuration

--- lib/counter/view/counter_page.dart
+++ lib/counter/view/counter_page.dart
@@ -1,14 +1,8 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kind_of_cool_ideas/counter/counter.dart';
-import 'package:kind_of_cool_ideas/l10n/l10n.dart';
+
+import '../../l10n/l10n.dart';
+import '../counter.dart';
 
 class CounterPage extends StatelessWidget {
   const CounterPage({super.key});
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,12 +1,5 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
-import 'package:kind_of_cool_ideas/app/app.dart';
-import 'package:kind_of_cool_ideas/bootstrap.dart';
+import 'app/app.dart';
+import 'bootstrap.dart';
 
 void main() {
   bootstrap(() => const App());
